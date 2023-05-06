e2ed5b8b792191534cac9b17976fe6a8c7edf1fb
Tanne <quachtienthanh92@gmail.com>
2023-05-06 13:05:36 +0200

    update get data validate form invalid

--- validation.js
+++ validation.js
@@ -1,114 +1,121 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Document</title>
-        <link rel="stylesheet" href="style.css" />
-    </head>
-    <body>
-        <div class="main">
-            <form action="" method="POST" class="form" id="form-1">
-                <h3 class="heading">Register Account Be Member</h3>
-                <p class="desc">Wellcome To My Projekt ❤️</p>
+function validator(options) {
+    var selectorRules = {};
+    function validate(inputElement, rule) {
+        var errorElement = rule.test(inputElement.value);
+        var rules = selectorRules[rule.selector];
+        for (let i = 0; i < rules.length; i++) {
+            errorElement = rules[i](inputElement.value);
+            if (errorElement) break;
+        }
 
-                <div class="spacer"></div>
+        var errorMessage = inputElement.parentElement.querySelector(
+            options.errorMessage
+        );
 
-                <div class="form-group">
-                    <label for="fullname" class="form-label">Full Name</label>
-                    <input
-                        id="fullname"
-                        name="fullname"
-                        type="text"
-                        placeholder="VD: Sofia Johansson"
-                        class="form-control"
-                    />
-                    <span class="form-message"></span>
-                </div>
+        if (errorElement) {
+            errorMessage.innerHTML = errorElement;
+            errorMessage.parentElement.classList.add("invalid");
+        } else {
+            errorMessage.innerHTML = "";
+            errorMessage.parentElement.classList.remove("invalid");
+        }
+        return !errorElement;
+    }
+    function removeError(errorMessage) {
+        errorMessage.innerHTML = "";
+        errorMessage.parentElement.classList.remove("invalid");
+    }
+    var elementForm = document.querySelector(options.form);
+    if (elementForm) {
+        elementForm.onsubmit = function (e) {
+            e.preventDefault();
 
-                <div class="form-group">
-                    <label for="email" class="form-label">Email</label>
-                    <input
-                        id="email"
-                        name="email"
-                        type="text"
-                        placeholder="VD: email@domain.com"
-                        class="form-control"
-                    />
-                    <span class="form-message"></span>
-                </div>
+            var isValidForm = true;
+            options.rules.forEach(function (rule) {
+                var inputElement = document.querySelector(rule.selector);
+                var isValid = validate(inputElement, rule);
 
-                <div class="form-group">
-                    <label for="password" class="form-label">Password</label>
-                    <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        placeholder="Write pass here"
-                        class="form-control"
-                    />
-                    <span class="form-message"></span>
-                </div>
-
-                <div class="form-group">
-                    <label for="password_confirmation" class="form-label"
-                        >Confirm Password</label
-                    >
-                    <input
-                        id="password_confirmation"
-                        name="password_confirmation"
-                        placeholder="Confirm again"
-                        type="password"
-                        class="form-control"
-                    />
-                    <span class="form-message"></span>
-                </div>
-
-                <button class="form-submit">Register Account</button>
-            </form>
-        </div>
-        <script src="./validationfake.js"></script>
-        <script>
-            validator({
-            
-                form: "#form-1",
-                errorMessage: ".form-message",
-                rules: [
-                    validator.isRequired(
-                        "#fullname",
-                        "Please write your full name :)"
-                    ),
-                    validator.isRequired(
-                        "#email",
-                        "Please write your email :)"
-                    ),
-                    validator.isEmail(
-                        "#email",
-                        "This format not a type of email"
-                    ),
-                    validator.isRequired(
-                        "#password",
-                        "Please do not let it empty:)"
-                    ),
-                    validator.minLength("#password", 6, "Minimum 6 char"),
-                    validator.isConfirmed(
-                        "#password_confirmation",
-                        function () {
-                            return document.querySelector("#form-1 #password")
-                                .value;
-                        },
-                        "Confirmed password is not correct"
-                    ),
-                    validator.isRequired(
-                        "#password_confirmation",
-                        "Please do not let it empty:)"
-                    ),
-                ],
-                onSubmit: function (data) {
-                    console.log(data);
-                },
+                if (!isValid) {
+                    isValidForm = false;
+                }
             });
-        </script>
-    </body>
-</html>
+
+            if (isValidForm) {
+                if (typeof options.onSubmit === "function") {
+                    var Enebleinput = elementForm.querySelectorAll("[name]");
+                    var formValues = Array.from(Enebleinput).reduce(function (
+                        value,
+                        input
+                    ) {
+                        value[input.name] = input.value;
+                        return value;
+                    },
+                    {});
+                    options.onSubmit({ formValues });
+                }
+            }
+        };
+    }
+
+    options.rules.forEach(function (rule) {
+        var inputElement = document.querySelector(rule.selector);
+        if (selectorRules[rule.selector]) {
+            selectorRules[rule.selector].push(rule.test);
+        } else {
+            selectorRules[rule.selector] = [rule.test];
+        }
+        if (inputElement) {
+            inputElement.onblur = function () {
+                validate(inputElement, rule);
+            };
+            var errorMessage = inputElement.parentElement.querySelector(
+                options.errorMessage
+            );
+            if (errorMessage) {
+                inputElement.oninput = function () {
+                    removeError(errorMessage);
+                };
+            }
+        }
+    });
+}
+
+validator.isRequired = function (selector, message) {
+    return {
+        selector: selector,
+        test: function (value) {
+            return value.trim() ? undefined : message;
+        },
+    };
+};
+validator.isEmail = function (selector) {
+    return {
+        selector: selector,
+        test: function (value) {
+            var regax = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            return regax.test(value)
+                ? undefined
+                : "Định dạng này không phải email";
+        },
+    };
+};
+validator.minLength = function (selector, min) {
+    return {
+        selector: selector,
+        test: function (value) {
+            return value.length <= min
+                ? undefined
+                : `Mật khẩu tối thiểu ${min} kí tự`;
+        },
+    };
+};
+validator.isConfirmed = function (selector, cb) {
+    return {
+        selector: selector,
+        test: function (value) {
+            return value === cb()
+                ? undefined
+                : `The password not the same please try again`;
+        },
+    };
+};
